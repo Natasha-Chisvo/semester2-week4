1accc5460d9eb4d6c2b8afccd487d86e9091effe
Natasha <146737058+Natasha-Chisvo@users.noreply.github.com>
2026-03-16 06:41:24 +0000

    All session 1 task 1 done

--- session-1/task1/vector_add.c
+++ session-1/task1/vector_add.c
@@ -9,11 +9,20 @@
     float a[]={ 1.0,1.0,1.0,1.0,1.0 };
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
     float c[5];
+    int k;
 
     /*
     Code to compute the sum
     Store your answer in array c
     Print your final answer
     */
+
+    for(k=0; k<5; k++){
+      c[k] = a[k] + b[k];
+    }
+
+    for(k=0; k<5; k++){
+    printf("answer %d : %f\n", k, c[k]);
+    }
     return 0;
  }
