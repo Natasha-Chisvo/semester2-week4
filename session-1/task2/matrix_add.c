d1a0bff8d3fd757d690b0a70265613225c714f30
Natasha <146737058+Natasha-Chisvo@users.noreply.github.com>
2026-03-16 07:00:24 +0000

    session 1 Task 2.1 done

--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -9,6 +9,8 @@
     float a[4][4];
     float b[4][4];
     float c[4][4];
+    int k;
+    int j;
 
     /*
     Intialise the matrix a and b entries to 1. 
@@ -16,5 +18,28 @@
     Store your answer in matrix c
     Print your final answer
     */
+  // initialise matrices a and b to 1
+    for(k = 0; k < 4; k++){
+        for(j = 0; j < 4; j++){
+            a[k][j] = 1;
+            b[k][j] = 1;
+        }
+    }
+
+    // compute matrix sum
+    for(k = 0; k < 4; k++){
+        for(j = 0; j < 4; j++){
+            c[k][j] = a[k][j] + b[k][j];
+        }
+    }
+
+    // print result
+    for(k = 0; k < 4; k++){
+        for(j = 0; j < 4; j++){
+            printf("%f : ", c[k][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
  }
