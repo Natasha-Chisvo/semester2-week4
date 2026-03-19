ac3bf0bc3317c723c9fcb7724da926c03de98454
Natasha <146737058+Natasha-Chisvo@users.noreply.github.com>
2026-03-19 18:10:43 +0000

    Worksheet 4 complete

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name:Natasha Chisvo
+Student ID: 201553125
 */
 
 #include <stdio.h>
@@ -11,7 +11,20 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
 
+    char buffer[100] = "";
+
     // process the command-line data using appropriate string functions
+
+    //* need to go through argv[1] --> agrv[argc-1]
+    for (int i = 1; i<argc; i++){
+        //join together values using strcat
+        strcat(buffer, argv[i]);
+
+        //*put a dash between each variable in buffer while i<argc-1
+        if (i<argc - 1){
+            strcat(buffer, "-");
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
